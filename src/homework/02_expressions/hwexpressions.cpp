d8620a147dc41c531788829d100a168aa933fd2a
Phieu-Pham <phieu.pham@g.austincc.edu>
2025-02-11 01:59:48 +0000

    homework 2 get sales tax and tip amount

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -1,5 +1,7 @@
 #include "hwexpressions.h"
 
+const auto tax_rate = 0.0675;
+
 //example
 int add_numbers(int num1, int num2)
 {
@@ -7,7 +9,18 @@ int add_numbers(int num1, int num2)
 }
 
 //write function code here
+double get_sales_tax_amount(double meal_amount)
+{
+	auto result = tax_rate * meal_amount;
 
+	return result;
+}
 
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+	auto result = meal_amount * tip_rate;
+
+	return result;
+}
 
 
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,11 +1,34 @@
 ﻿//write include statements
+#include <iostream>
+#include "hwexpressions.h"
 
+using std::cin;
+using std::cout;
 
 //write namespace using statement for cout
 
 int main()
 {
-	
+	double meal_amount;
+	double tip_rate;
+	double tip_amount;
+	double tax_amount;
+	double total;
+
+	cout << "Enter meal amount: ";
+	cin >> meal_amount;
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cout << "Enter tip rate: ";
+	cin >> tip_rate;
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	total = meal_amount + tax_amount + tip_amount;
+
+	cout << "Meal Amount: " << meal_amount << "\n";
+	cout << "Sales Tax: " << tax_amount << "\n";
+	cout << "Tip Amount: " << tip_amount << "\n";
+	cout << "Total: " << total << "\n";
 
 	return 0;
 }
