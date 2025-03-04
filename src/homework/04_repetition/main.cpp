9f3b3d76aa27eb5e1457db841a77f2d1a16a63b0
Phieu-Pham <phieu.pham@g.austincc.edu>
2025-03-04 01:23:53 +0000

    find factorial and greatest common divisor

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,51 @@
 //write include statements
+#include <iostream>
+#include "repetition.h"
+
+using std::cout;
+using std::cin;
+using std::string;
 
 //write using statements
 
 int main() 
 {
-	return 0;
+	int selection;
+	string exit_selection;
+	int number;
+	int number1;
+	int number2;
+	int result;
+
+	do
+	{
+		cout << "1-Factorial" << "\n";
+		cout << "2-Greatest Common Divisor" << "\n";
+		cout << "3-Exit" << "\n";
+		cout << "Enter your selection: ";
+		cin >> selection;
+
+	} while (selection != 1 && selection != 2 && selection != 3);
+
+	if (selection == 1){
+		cout << "Enter a number: ";
+		cin >> number;
+		result = factorial(number);
+		cout << "Factorial of " << number << " is: " << result << "\n";
+	}
+
+	if (selection == 2){
+		cout << "Enter the first number: ";
+		cin >> number1;
+		cout << "Enter the second number: ";
+		cin >> number2;
+		result = gcd(number1, number2);
+		cout << "Greatest common divisor of " << number1 << " and " << number2 << " is: " << result << "\n";
+	}
+	
+	if (selection == 3){
+		cout << "Are you sure you want to exit? \n";
+	}
+
+	return result;
 }
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,32 @@
 //add include statements
+#include "repetition.h"
 
 //add function(s) code here
+int factorial(int num){
+    int result = 1;
+
+    while (num > 0){
+        result = result * num;
+        num -= 1;
+    }
+    
+    return result;
+}
+
+int gcd(int num1, int num2){
+
+    while (num1 != num2){
+
+        if (num1 < num2){
+            int num3 = num1;
+            num1 = num2;
+            num2 = num3;
+        }
+
+        if (num1 > num2){
+            num1 = num1 - num2;
+        }
+    }
+
+    return num1;
+}
